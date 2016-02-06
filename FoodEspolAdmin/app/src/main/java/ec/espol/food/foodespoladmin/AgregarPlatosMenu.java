2ebb6f124eeb3a4c8851797165aa2e859885e17a
jorenver <jorenver@espol.edu.ec>
2016-02-06 14:58:17 -0500

    creacion de nuevas actividades y edicion de nombre de botones, listas

---
+++ FoodEspolAdmin/app/src/main/java/ec/espol/food/foodespoladmin/AgregarPlatosMenu.java
@@ -0,0 +1,13 @@
+package ec.espol.food.foodespoladmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarPlatosMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_platos_menu);
+    }
+}
